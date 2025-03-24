926c2ffb57e82460822f4166595daafe0388f726
Ignacio Donoso <naxonicolas1997@gmail.com>
2025-03-24 20:07:56 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const tasks = [
+    {
+        id: 1,
+        name: "Task 1",
+    },
+    {
+        id: 2,
+        name: "Task 2",
+    },
+];
+
+app.get("/tasks", (req, res) => {
+    return res.json(tasks);
+});
+
+app.get("/tasks/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const task = tasks.find((task) => task.id === id);
+    if (task === null) return res.status(404).send();
+    return res.json(task);
+});
+
+app.listen(port, () => {
+    console.log("Server running...");
+});
+
+module.exports = app;
